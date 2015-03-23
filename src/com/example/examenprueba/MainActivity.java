3953e1460a8eb1d4a7947d9edf7aae8fbe464903
Johan <jfaridgarcia@hotmail.com>
2015-03-23 10:48:06 +0100

    Branca2

--- src/com/example/examenprueba/MainActivity.java
+++ src/com/example/examenprueba/MainActivity.java
@@ -7,7 +7,7 @@ import android.view.MenuItem;
 
 
 public class MainActivity extends Activity {
-//BRANCA
+//BRANCA2
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
